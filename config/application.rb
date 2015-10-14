0e83c09316e916600f2a672c3a6eba4215177915
nekocreate <nekocreate7@gmail.com>
2015-10-14 02:48:01 +0000

    config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Olddomain
     config.i18n.default_locale = :ja
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.serve_static_assets = true
   end
 end
